cb4a13833052b8995459bd6ae66abcabdaa13707
Maharaj Hossain Tanim <128939036+MaharajTanim@users.noreply.github.com>
2025-05-27 03:53:35 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ✅ Important for Netlify deployment
   server: {
     host: true,
     hmr: {
       clientPort: 443,
-    }
-  }
+    },
+  },
 });
